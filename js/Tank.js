99de6bd8826a0236f11da4dbb3b0658be467b8ed
P-Jinseok <pjs5609@gmail.com>
2026-07-24 14:02:31 +0900

    Update Tank.js

--- js/Tank.js
+++ js/Tank.js
@@ -1,9 +1,9 @@
 export class Tank {
     constructor(id, tankType, x, color, darkColor) {
         this.id = id;
-        this.tankType = tankType; // CATAPULT, CANNON, MISSILE, DUKE, MULTI, LASER
+        this.tankType = tankType;
         this.x = x;
-        this.y = 300;
+        this.y = 50; // 초기 위치를 공중 위쪽에서 떨어지도록 안전 설정
         this.hp = 100;
         this.maxHp = 100;
         this.angle = 45;
@@ -16,32 +16,27 @@ export class Tank {
         this.animFrame = 0;
     }
 
-    reset(x) {
-        this.x = x;
-        this.y = 300;
-        this.hp = 100;
-        this.angle = 45;
-        this.weapon = 1;
-        this.moveEnergy = 100;
-        this.recoil = 0;
-        this.isFallen = false;
-    }
-
     update(terrain) {
         if (this.isFallen) return;
 
         this.animFrame++;
 
         const groundY = terrain.getTerrainY(this.x);
+        
+        // 지형이 완전히 없는 허공인 경우 추락
         if (groundY === null) {
-            this.y += 5;
+            this.y += 6;
             if (this.y > 620) {
                 this.hp = 0;
                 this.isFallen = true;
             }
         } else {
-            if (this.y < groundY) this.y += 3.5;
-            else this.y = groundY;
+            // 지형 높이로 자연스럽게 착지
+            if (this.y < groundY) {
+                this.y = Math.min(this.y + 6, groundY);
+            } else if (this.y > groundY) {
+                this.y = groundY;
+            }
         }
 
         if (this.recoil > 0) this.recoil -= 0.5;
@@ -57,22 +52,18 @@ export class Tank {
         const rad = this.angle * Math.PI / 180;
         const recoilOffset = this.recoil || 0;
 
-        // 💡 6종 탱크 각각의 디자인 표현
         if (this.tankType === 'CATAPULT') {
-            // [1. 인민탱크 (Catapult)] 투석기 형태
             ctx.fillStyle = '#0f172a';
             ctx.beginPath(); ctx.roundRect(-22, -3, 44, 12, 5); ctx.fill();
 
-            ctx.fillStyle = '#854d0e'; // 목재/인민 카키 프레임
+            ctx.fillStyle = '#854d0e';
             ctx.fillRect(-14, -14, 28, 11);
 
-            // 투석기 프레임 포신
             const bLen = 28 - recoilOffset;
             ctx.strokeStyle = '#a16207'; ctx.lineWidth = 6;
             ctx.beginPath(); ctx.moveTo(-6, -10); ctx.lineTo(Math.cos(rad) * bLen, -10 - Math.sin(rad) * bLen); ctx.stroke();
 
         } else if (this.tankType === 'CANNON') {
-            // [2. 캐논] 정통 캐논 포탑
             ctx.fillStyle = '#0f172a';
             ctx.beginPath(); ctx.roundRect(-20, -2, 40, 11, 5); ctx.fill();
 
@@ -84,7 +75,6 @@ export class Tank {
             ctx.beginPath(); ctx.moveTo(0, -6); ctx.lineTo(Math.cos(rad) * bLen, -6 - Math.sin(rad) * bLen); ctx.stroke();
 
         } else if (this.tankType === 'MISSILE') {
-            // [3. 유도 미사일 탱크] 미사일 포대 형태
             ctx.fillStyle = '#0f172a';
             ctx.beginPath(); ctx.roundRect(-22, -3, 44, 12, 5); ctx.fill();
 
@@ -96,7 +86,6 @@ export class Tank {
             ctx.beginPath(); ctx.moveTo(0, -10); ctx.lineTo(Math.cos(rad) * bLen, -10 - Math.sin(rad) * bLen); ctx.stroke();
 
         } else if (this.tankType === 'DUKE') {
-            // [4. 듀크 탱크] 묵직한 오버사이즈 장갑
             ctx.fillStyle = '#0f172a';
             ctx.beginPath(); ctx.roundRect(-26, -4, 52, 14, 6); ctx.fill();
 
@@ -108,7 +97,6 @@ export class Tank {
             ctx.beginPath(); ctx.moveTo(0, -11); ctx.lineTo(Math.cos(rad) * bLen, -11 - Math.sin(rad) * bLen); ctx.stroke();
 
         } else if (this.tankType === 'MULTI') {
-            // [5. 멀티 탱크] 다련장 포신
             ctx.fillStyle = '#0f172a';
             ctx.beginPath(); ctx.roundRect(-20, -3, 40, 12, 5); ctx.fill();
 
@@ -117,12 +105,10 @@ export class Tank {
 
             const bLen = 28 - recoilOffset;
             ctx.strokeStyle = '#15803d'; ctx.lineWidth = 3;
-            // 3연사 이중 포신 느낌
             ctx.beginPath(); ctx.moveTo(-2, -8); ctx.lineTo(Math.cos(rad) * bLen - 2, -8 - Math.sin(rad) * bLen); ctx.stroke();
             ctx.beginPath(); ctx.moveTo(2, -4); ctx.lineTo(Math.cos(rad) * bLen + 2, -4 - Math.sin(rad) * bLen); ctx.stroke();
 
         } else if (this.tankType === 'LASER') {
-            // [6. 레이저 탱크] SF 하이테크 형태
             ctx.fillStyle = '#0f172a';
             ctx.beginPath(); ctx.roundRect(-20, -3, 40, 12, 5); ctx.fill();
 
